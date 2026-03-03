9e6270343261cfb2b878eb1cdf364ce9daf462d6
Youngjin Mo <dev.youngjinmo@gmail.com>
2026-03-03 21:38:23 +0900

    Refactor. Integrate duplication pretter/eslint configuration

--- front-end/eslint.config.js
+++ front-end/eslint.config.js
@@ -4,11 +4,11 @@ import reactHooks from 'eslint-plugin-react-hooks';
 import reactRefresh from 'eslint-plugin-react-refresh';
 import tseslint from 'typescript-eslint';
 import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
+import { createBaseConfig } from '../eslint.config.base.mjs';
 
 export default tseslint.config(
-  { ignores: ['dist'] },
+  ...createBaseConfig({ js, tseslint, eslintPluginPrettierRecommended }),
   {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
     files: ['**/*.{ts,tsx}'],
     languageOptions: {
       ecmaVersion: 2020,
@@ -21,23 +21,8 @@ export default tseslint.config(
     rules: {
       ...reactHooks.configs.recommended.rules,
       'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
-      '@typescript-eslint/no-unused-vars': [
-        'error',
-        {
-          argsIgnorePattern: '^_',
-          varsIgnorePattern: '^_',
-          caughtErrorsIgnorePattern: '^_',
-        },
-      ],
-      '@typescript-eslint/no-explicit-any': 'off',
       '@typescript-eslint/no-empty-object-type': 'off',
       '@typescript-eslint/no-require-imports': 'off',
-    },
-  },
-  eslintPluginPrettierRecommended,
-  {
-    rules: {
-      'prettier/prettier': ['error', { endOfLine: 'auto' }],
     },
   },
 );
