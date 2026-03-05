993ce543d8ddf97b92d08c2aa5e07fb182ce47fe
Youngjin Mo <dev.youngjinmo@gmail.com>
2026-03-05 21:22:59 +0900

    Refactor. refactor ClientUtil and AuthController

--- back-end/src/auth/auth.controller.ts
+++ back-end/src/auth/auth.controller.ts
@@ -21,14 +21,14 @@ import { REFRESH_TOKEN_EXPIRATION } from './auth.policy';
 import { Public } from '../common/decorators/public.decorator';
 import { CurrentUser, type CurrentUserPayload } from '../common/decorators/current-user.decorator';
 import { LoginDto } from './dto/login.dto';
-import { ClientUtil } from 'src/common/utils/client.util';
+import { getClientInfo } from 'src/common/utils/client.util';
+import { REFRESH_TOKEN_TTL } from 'src/common/utils/policy';
 
 @Controller('auth')
 export class AuthController {
   constructor(
     private readonly authService: AuthService,
     private readonly oauthService: OAuthService,
-    private readonly clientUtil: ClientUtil,
   ) {}
 
   @Public()
@@ -49,7 +49,7 @@ export class AuthController {
     @Req() request: Request,
     @Res({ passthrough: true }) response: Response,
   ): Promise<Pick<AuthResponseDto, 'accessToken'>> {
-    const { ip, userAgent } = this.clientUtil.getClientInfo(request);
+    const { ip, userAgent } = getClientInfo(request);
     const { accessToken, refreshToken } = await this.authService.verifyCodeAndLogin(
       dto,
       ip,
@@ -72,7 +72,7 @@ export class AuthController {
       throw new UnauthorizedException('Refresh token not found');
     }
 
-    const { ip, userAgent } = this.clientUtil.getClientInfo(request);
+    const { ip, userAgent } = getClientInfo(request);
     const { accessToken, refreshToken: newRefreshToken } = await this.authService.refreshTokens(
       refreshToken,
       ip,
@@ -119,7 +119,7 @@ export class AuthController {
     @Req() request: Request,
     @Res({ passthrough: true }) response: Response,
   ): Promise<Pick<AuthResponseDto, 'accessToken'>> {
-    const { ip, userAgent } = this.clientUtil.getClientInfo(request);
+    const { ip, userAgent } = getClientInfo(request);
     const { accessToken, refreshToken } = await this.oauthService.loginWithGithub(
       dto.code,
       dto.redirectUri,
@@ -139,7 +139,7 @@ export class AuthController {
     @Req() request: Request,
     @Res({ passthrough: true }) response: Response,
   ): Promise<Pick<AuthResponseDto, 'accessToken'>> {
-    const { ip, userAgent } = this.clientUtil.getClientInfo(request);
+    const { ip, userAgent } = getClientInfo(request);
     const { accessToken, refreshToken } = await this.oauthService.loginWithGoogle(
       dto.code,
       dto.redirectUri,
@@ -159,7 +159,7 @@ export class AuthController {
     @Req() request: Request,
     @Res({ passthrough: true }) response: Response,
   ): Promise<Pick<AuthResponseDto, 'accessToken'>> {
-    const { ip, userAgent } = this.clientUtil.getClientInfo(request);
+    const { ip, userAgent } = getClientInfo(request);
     const { accessToken, refreshToken } = await this.oauthService.loginWithApple(
       dto.idToken,
       ip,
